4015abf8c2bc42bc6dcab8dc84f91cc795094c82
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-02-06 10:56:06 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -1,16 +1,30 @@
-
-import eel
-
-# getcwdにより初期フォルダ確認済み
-eel.init("eel_project/web")
-eel.start("index.html")
-
-# javascript側から呼び出す
-@eel.expose
-def test_python_func():
-    return "Test"
-
- 
+import pandas as pd
 
 
+### デスクトップアプリ作成課題
 
+def kimetsu_search(word):
+    # 検索対象取得
+    # https://note.nkmk.me/python-pandas-read-csv-tsv/　read_csv説明
+    df=pd.read_csv("C:/Users/Yoshi/Project2/eel_project/source.csv")
+    # detaframeのnameカラムをリスト化。souurceというリストにvalueを入れていく
+    source=list(df["name"])
+
+    # 検索
+    if word in source:
+        print("『{}』はあります".format(word))
+   
+
+    else:
+        print("『{}』はありません".format(word))
+   
+        # 追加
+        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        #if add_flg=="1":
+        source.append(word)
+    
+    # CSV書き込み
+    df=pd.DataFrame(source,columns=["name"])
+    df.to_csv("C:/Users/Yoshi/Project2/eel_project/source.csv",encoding="utf_8-sig")
+    print(source)
+    return word
